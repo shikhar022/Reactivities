a74c113231648e2f06c4116c7cc80d412b8f3615
Srivastava, Shikhar (Agoda) <shikhar.srivastava@agoda.com>
2021-12-06 22:07:08 +0530

    React component additon

--- API/Startup.cs
+++ API/Startup.cs
@@ -25,17 +25,18 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
+        public void ConfigureServices(IServiceCollection services) {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
+            services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
-            services.AddDbContext<DataContext>( opt => 
-            {       
+            services.AddDbContext<DataContext>( opt => {       
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
             });
         }
 
@@ -52,6 +53,7 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors("CorsPolicy"); // ordering is very necessary just after use routing function
 
             app.UseAuthorization();
 
---
+++ client-app/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { Header, List } from "semantic-ui-react";
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then((resp) => {
+      console.log(resp);
+      setActivities(resp.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Header as="h2" icon="users" content="Reactivities"></Header>
+      <List>
+        {activities.map((activity: any) => {
+          return <List.Item key={activity.id}>{activity.title}</List.Item>;
+        })}
+      </List>
+    </div>
+  );
+}
+
+export default App;
